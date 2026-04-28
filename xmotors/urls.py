8fdcedbf5bcc567808c7e104a04388a2f40a383f
ogGEORGE-x <gfurious01@gmail.com>
2026-04-28 16:51:27 +0300

    Index modified

--- xmotors/urls.py
+++ xmotors/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('car/', views.car, name='car'),
+    path('car2/', views.car2, name='car2'),
+    path('car3/', views.car3, name='car3'),
+    path('carfl/', views.carfl, name='carfl'),
     path('car-details/', views.cardetails, name='car-details'),
     path('blog/', views.blog, name='blog'),
     path('blog-details/', views.blogdetails, name='blog-details'),
     path('add_car/', views.add_car, name='add_car'),
+    path('bmw/', views.bmw, name='bmw'),
 ]
--- xmotors/views.py
+++ xmotors/views.py
@@ -9,6 +9,12 @@ def contact(request):
     return render(request, 'contact.html') 
 def car(request):
     return render(request, 'car.html')
+def car2(request):
+    return render(request, 'car2.html')
+def car3(request):
+    return render(request, 'car3.html')
+def carfl(request):
+    return render(request, 'carfl.html')
 def cardetails(request):
     return render(request, 'car-details.html')  
 def blog(request):
@@ -25,3 +31,5 @@ def add_car(request):
         # You can add code here to save the car details to your database
         # For now, we will just render the same page after submission
     return render(request, 'add_car.html')
+def bmw(request):
+    return render(request, 'bmw.html')
